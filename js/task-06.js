24e3c81e1bac7c3d6bcfc5db42dee6847975a944
Nataliia Pogrebna <natalibunny13@gmail.com>
2021-08-30 16:00:23 +0300

    task06

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,14 @@
 const textInput = document.querySelector("#validation-input");
 const validNumber = Number(textInput.dataset.length);
-textInput.addEventListener("focus", () => {
-    
+textInput.addEventListener("input", () => {
+  textInput.classList.remove("invalid");
+  textInput.classList.remove("valid");
   });
   
 textInput.addEventListener("blur", () => {
     if (textInput.value.length !== validNumber) {
         textInput.classList.add("invalid");
     }  else {
-        textInput.classList.remove("invalid")
         textInput.classList.add("valid");
     }
   });
