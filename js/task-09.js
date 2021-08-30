5b6213318883f4cffde441828c6a9e8a4845a395
Nataliia Pogrebna <natalibunny13@gmail.com>
2021-08-30 11:33:39 +0300

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
+const changeColor = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const spanColor = document.querySelector(".color");
+
+changeColor.addEventListener("click", getRandomHexColor);
+
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return spanColor.textContent = `${body.style.backgroundColor}`;
 }
+
