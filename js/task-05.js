e051cc346b07408dc58dfaf809043416b671d82a
Nataliia Pogrebna <natalibunny13@gmail.com>
2021-08-30 15:57:51 +0300

    task05

--- js/task-05.js
+++ js/task-05.js
@@ -4,5 +4,9 @@ console.log(inputName.textContent);
 console.log(inputAdd.textContent);
 
 inputName.addEventListener("input", (event) => {
+  if (event.currentTarget.value !== "") {
     inputAdd.textContent = event.currentTarget.value;
+  } else {
+    inputAdd.textContent = "Anonymous";
+  }
   });
